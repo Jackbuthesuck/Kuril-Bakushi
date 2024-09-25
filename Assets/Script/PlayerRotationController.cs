7a88199b9ee4f68fa2577508d4faf645b3ce3d9f
Jackbuthesuck <poonwasu2548@gmail.com>
2024-09-25 17:19:38 +0700

    Angle is so fun

--- Assets/Script/PlayerRotationController.cs
+++ Assets/Script/PlayerRotationController.cs
@@ -12,7 +12,7 @@ public class PlayerRotationController : MonoBehaviour
     public float lerpStrenght;
 
     private float mathAngle = 0;
-    private float targetAngle= 0;
+    private float targetAngle = 0;
 
     Vector2 moveDirection = Vector2.zero;
 
@@ -41,8 +41,9 @@ public class PlayerRotationController : MonoBehaviour
         transform.position = new Vector3(playerPosition.position.x, 0.5f + playerPosition.position.y, playerPosition.position.z);
 
         Vector2 lookVector = look.ReadValue<Vector2>();
-        if (lookVector != Vector2.zero)   targetAngle = Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg;
-        if (lookVector != Vector2.zero)   rb.MoveRotation(Quaternion.Euler(0, targetAngle, 0));
+        mathAngle = Mathf.Lerp(mathAngle, targetAngle, lerpStrenght);
+        if (lookVector != Vector2.zero) targetAngle = Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg;
+        rb.MoveRotation(Quaternion.Euler(0, mathAngle, 0));
     }
 
     private void FixedUpdate()
@@ -53,6 +54,7 @@ public class PlayerRotationController : MonoBehaviour
     {
         GUIStyle style = new GUIStyle();
         style.fontSize = 18;
-        GUI.Label(new Rect(10, 10, 0, 0), "rot y: " + transform.eulerAngles.y, style);
+        GUI.Label(new Rect(10, 30, 0, 0), "Math Angle: " + mathAngle, style);
+        GUI.Label(new Rect(10, 50, 0, 0), "Targ Angle: " + targetAngle, style);
     }
 }
