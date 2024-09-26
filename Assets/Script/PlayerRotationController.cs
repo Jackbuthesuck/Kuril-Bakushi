f456057010f2132ac799d06f6050e9f39289e7b6
Jackbuthesuck <poonwasu2548@gmail.com>
2024-09-27 04:36:48 +0700

    We are so gaming

--- Assets/Script/PlayerRotationController.cs
+++ Assets/Script/PlayerRotationController.cs
@@ -10,12 +10,14 @@ public class PlayerRotationController : MonoBehaviour
     private InputAction look;
 
     public float lookSpeed;
+    public float minForce;
+    public float lerpStrenght;
 
     private float mathAngle = 0;
     private float targetAngle = 0;
 
     Vector2 moveDirection = Vector2.zero;
-
+    Vector2 lookVector = Vector2.zero;
     private void Awake()
     {
         playerControl = new PlayerInputAction();
@@ -40,10 +42,12 @@ public class PlayerRotationController : MonoBehaviour
     {
         transform.position = new Vector3(playerPosition.position.x, 0.5f + playerPosition.position.y, playerPosition.position.z);
 
-        Vector2 lookVector = look.ReadValue<Vector2>();
+        lookVector = look.ReadValue<Vector2>();
         if (mathAngle < targetAngle) mathAngle += lookSpeed;
         else mathAngle -= lookSpeed;
-        if (lookVector != Vector2.zero) targetAngle = Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg;
+        if (lookVector.x > minForce || lookVector.y > minForce || lookVector.x < -minForce || lookVector. y < -minForce)
+            targetAngle = Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg;
+        mathAngle = Mathf.LerpAngle(mathAngle, targetAngle, lerpStrenght);
         rb.MoveRotation(Quaternion.Euler(0, mathAngle, 0));
     }
 
@@ -57,5 +61,7 @@ public class PlayerRotationController : MonoBehaviour
         style.fontSize = 18;
         GUI.Label(new Rect(10, 30, 0, 0), "Math Angle: " + mathAngle, style);
         GUI.Label(new Rect(10, 50, 0, 0), "Targ Angle: " + targetAngle, style);
+        GUI.Label(new Rect(10, 70, 0, 0), "Look X: " + lookVector.x, style);
+        GUI.Label(new Rect(10, 90, 0, 0), "Look Y: " + lookVector.y, style);
     }
 }
